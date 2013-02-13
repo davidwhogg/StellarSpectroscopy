6d90c97f7db1bbec99a5a7a3abd9855d46c44f16
Jeffrey Mei <mym226@nyu.edu>
2013-02-13 19:41:29 +0100

    updated code
    it has a step function and __main__

--- hoggintro.py
+++ hoggintro.py
@@ -8,22 +8,27 @@ from pylab import *
 from scipy import *
 import matplotlib.pyplot as plt
 
-plateid=input("Plate ID (e.g. 276): ")
-mjd=input("mjd (e.g. 51909): ")
-fiber=input("fiber (e.g. 627): ")
+def __main__():
+    
+    plateid=input("Plate ID (e.g. 276): ")
+    mjd=input("mjd (e.g. 51909): ")
+    fiber=input("fiber (e.g. 627): ")
 
-commands.getoutput('wget --content-disposition "http://api.sdss3.org/spectrum?plate='+str(plateid)+'&fiber='+str(fiber)+'&mjd='+str(mjd)+'"')
+    commands.getoutput('wget --content-disposition "http://api.sdss3.org/spectrum?plate='+str(plateid)+'&fiber='+str(fiber)+'&mjd='+str(mjd)+'"')
 
-tab = pyfits.getdata(commands.getoutput("pwd")+'/spec-'+str(plateid).zfill(4)+'-'+str(mjd)+'-'+str(fiber).zfill(4)+'.fits')
+    tab = pyfits.getdata(commands.getoutput("pwd")+'/spec-'+str(plateid).zfill(4)+'-'+str(mjd)+'-'+str(fiber).zfill(4)+'.fits')
 
-flux=tab.field('flux')
-loglam=tab.field('loglam')
+    flux=tab.field('flux')
+    loglam=tab.field('loglam')
 
-lam=10**loglam
+    lam=10**loglam
 
 
-plt.plot(lam,flux,'.')
-plt.xlabel('Wavelength [Angstroms]')
-plt.ylabel('Flux [10^-17 erg/cm^2/s/Angstrom]')
-savefig(str(plateid).zfill(4)+'-'+str(mjd)+'-'+str(fiber).zfill(4)+'.png')
-show()
+    plt.step(lam,flux)
+    plt.xlabel('Wavelength [Angstroms]')
+    plt.ylabel('Flux [10^-17 erg/cm^2/s/Angstrom]')
+    savefig(str(plateid).zfill(4)+'-'+str(mjd)+'-'+str(fiber).zfill(4)+'.png')
+    show()
+    
+__main__()
+    
