43abf6fd88d9bceb42786aa90a439c04e086b026
Jeffrey Mei <mym226@nyu.edu>
2013-04-29 22:41:44 +0200

    no changes since last commit

--- assembled_data.py
+++ assembled_data.py
@@ -13,10 +13,10 @@ import matplotlib.pyplot as plt
 #sys.path.append(directory)
 import sqlcl
 
-def __main__():
+#def __main__():
+for i in range(1):
 
-
-    #below section just downloads the fits files
+    #below 2 lines just downloads the fits files
     commands.getoutput('wget --content-disposition "http://api.sdss3.org/spectrum?plate=2236&fiber=16&mjd=53729"')
     commands.getoutput('wget --content-disposition "http://api.sdss3.org/spectrum?plate=2231&fiber=10&mjd=53816"')
 
@@ -32,7 +32,11 @@ def __main__():
     wavelengths=[]
     correctedwave=[]
     objids=[]
+    logg=[]
+    teff=[]
+    feh=[]
     extvalues=[]
+    
 
     #below section sorts the data into the lists from above
     for i in range(0,2):
@@ -65,7 +69,41 @@ def __main__():
         for j in range(len(flux)): #redshift correction
             lamcor[j]=float(lam[j])/float((1+zm))
         correctedwave.append(lamcor)
-        
+
+            
+        alldata= sqlcl.query("SELECT top 100 p.objID, \
+        p.extinction_r, s.elodieTEff, s.elodieFeH, s.ElodieLogG \
+        FROM PhotoObj AS p \
+        JOIN SpecObj as s ON s.specobjID=p.specobjID \
+        WHERE psfMag_r<19  \
+        and psfMag_r>15 and type=6 \
+        and  dbo.fPhotoStatus('PRIMARY')>0 and dbo.fPhotoFlags('STATIONARY')>0 \
+        and calibStatus_r=1 \
+        and s.elodieTEff!=0 and s.elodieFeH!=0 and s.elodieLogG!=0 \
+        and ((flags&dbo.fPhotoFlags('BLENDED')) \
+        +(flags&dbo.fPhotoFlags('DEBLEND_TOO_MANY_PEAKS')) + \
+        (flags&dbo.fPhotoFlags('SATURATED')) \
+        +(flags&dbo.fPhotoFlags('BADSKY'))+ \
+        (flags&dbo.fPhotoFlags('COSMIC_RAY')) \
+        +(flags&dbo.fPhotoFlags('PEAKS_TOO_CLOSE'))+ \
+        (flags&dbo.fPhotoFlags('NOTCHECKED_CENTER')) \
+        +(flags&dbo.fPhotoFlags('SATUR_CENTER'))+ \
+        (flags&dbo.fPhotoFlags('INTERP_CENTER')) \
+        +(flags&dbo.fPhotoFlags('INTERP'))+ \
+        (flags&dbo.fPhotoFlags('PSF_FLUX_INTERP')))=0 \
+        and sqrt((power(psfMag_u-psfmag_g-0.82,2)+ \
+        power(psfMag_g-psfMag_r-0.3,2)+ \
+        power(psfMag_r-psfMag_i-0.09,2)+\
+        power(psfMag_i-psfMag_z-0.02,2)))<0.08").read()
+        interim=alldata.replace("\n",",")
+        compiled=interim.split(",")
+        for i in range(7,len(compiled)-1,5):
+            teff.append(compiled[i])
+        for i in range(8,len(compiled)-1,5):
+            feh.append(compiled[i])
+        for i in range(9,len(compiled)-1,5):
+            logg.append(compiled[i])
+            
         extinctions= sqlcl.query("SELECT top 1 objID, extinction_u, extinction_r, \
         extinction_g, extinction_z, extinction_i FROM PhotoObj WHERE objID="+objid).read()
         extinctions_split=extinctions.split("\n")
@@ -77,5 +115,5 @@ def __main__():
             ext=extinctions_values[1:6]
             extvalues.append(ext) #five values in each array; UGRIZ
 
-__main__()
+#__main__()
 
--- filterstars.py
+++ filterstars.py
@@ -1,25 +1,33 @@
+
 import sys
-sys.path.append('/Users/admin/Desktop/Maser files')
+directory=commands.getoutput("pwd")
+sys.path.append(directory)
 import sqlcl
 
-print sqlcl.query("SELECT top 10 objID, psfMag_r, psfFlux_r, extinction_r \
-FROM PhotoObj WHERE psfMag_r<19 \
+alldata= sqlcl.query("SELECT top 1000 p.objID, s.elodieObject, \
+p.extinction_g, s.elodieTEff, s.elodieFeH \
+FROM PhotoObj AS p \
+JOIN SpecObj as s ON s.specobjID=p.specobjID \
+WHERE psfMag_r<19  \
 and psfMag_r>15 and type=6 \
-and  dbo.fPhotoStatus('PRIMARY')>0\
-and dbo.fPhotoFlags('STATIONARY')>0\
-and calibStatus_r=1\
-and ((flags&dbo.fPhotoFlags('BLENDED'))\
-+(flags&dbo.fPhotoFlags('DEBLEND_TOO_MANY_PEAKS'))+\
-(flags&dbo.fPhotoFlags('SATURATED'))\
-+(flags&dbo.fPhotoFlags('BADSKY'))+\
-(flags&dbo.fPhotoFlags('COSMIC_RAY'))\
-+(flags&dbo.fPhotoFlags('PEAKS_TOO_CLOSE'))+\
-(flags&dbo.fPhotoFlags('NOTCHECKED_CENTER'))\
-+(flags&dbo.fPhotoFlags('SATUR_CENTER'))+\
-(flags&dbo.fPhotoFlags('INTERP_CENTER'))\
-+(flags&dbo.fPhotoFlags('INTERP'))+\
-(flags&dbo.fPhotoFlags('PSF_FLUX_INTERP')))=0\
-and sqrt((power(psfMag_u-psfmag_g-0.82,2)+\
-power(psfMag_g-psfMag_r-0.3,2)+\
+and  dbo.fPhotoStatus('PRIMARY')>0 and dbo.fPhotoFlags('STATIONARY')>0 \
+and calibStatus_r=1 \
+and s.elodieTEff!=0 and s.elodieFeH!=0 and s.elodieLogG!=0 \
+and ((flags&dbo.fPhotoFlags('BLENDED')) \
++(flags&dbo.fPhotoFlags('DEBLEND_TOO_MANY_PEAKS')) + \
+(flags&dbo.fPhotoFlags('SATURATED')) \
++(flags&dbo.fPhotoFlags('BADSKY'))+ \
+(flags&dbo.fPhotoFlags('COSMIC_RAY')) \
++(flags&dbo.fPhotoFlags('PEAKS_TOO_CLOSE'))+ \
+(flags&dbo.fPhotoFlags('NOTCHECKED_CENTER')) \
++(flags&dbo.fPhotoFlags('SATUR_CENTER'))+ \
+(flags&dbo.fPhotoFlags('INTERP_CENTER')) \
++(flags&dbo.fPhotoFlags('INTERP'))+ \
+(flags&dbo.fPhotoFlags('PSF_FLUX_INTERP')))=0 \
+and sqrt((power(psfMag_u-psfmag_g-0.82,2)+ \
+power(psfMag_g-psfMag_r-0.3,2)+ \
 power(psfMag_r-psfMag_i-0.09,2)+\
 power(psfMag_i-psfMag_z-0.02,2)))<0.08").read()
+
+# filters for calibration stars taken from
+# http://www.sdss3.org/dr9/algorithms/boss_std_ts.php
