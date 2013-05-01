6432ad4b4b53455ea5a76a3521e796f11e9cf1ee
Jeffrey Mei <mym226@nyu.edu>
2013-05-01 12:10:25 +0200

    fixed bug, added new fields to extract fluxes

--- sqldata.py
+++ sqldata.py
@@ -2,7 +2,10 @@ import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from pylab import *
+
 import commands
+import pyfits
 import sys
 directory=commands.getoutput("pwd")
 sys.path.append(directory)
@@ -20,9 +23,9 @@ camcol=[]
 run=[]
 ids=[]
 
-alldata= sqlcl.query("SELECT top 1000 p.objID, \
+alldata= sqlcl.query("SELECT top 10 p.objID, \
 p.extinction_g, s.elodieTEff, s.elodieFeH, s.elodieObject, p.camcol, p.run, p.field, \
-p.obj, s.plate, s.fiberID, s.mjd\
+p.obj, s.plate, s.fiberID, s.mjd \
 FROM PhotoObj AS p \
 JOIN SpecObj as s ON s.specobjID=p.specobjID \
 WHERE psfMag_r<19  \
@@ -47,28 +50,38 @@ power(psfMag_r-psfMag_i-0.09,2)+\
 power(psfMag_i-psfMag_z-0.02,2)))<0.08").read()
 interim=alldata.replace("\n",",")
 compiled=interim.split(",")
-for i in range(12,len(compiled)-1,11):
+for i in range(13,len(compiled)-1,12):
     extinction.append(compiled[i])
-for i in range(13,len(compiled)-1,11):
+for i in range(14,len(compiled)-1,12):
     teff.append(compiled[i])
-for i in range(14,len(compiled)-1,11):
+for i in range(15,len(compiled)-1,12):
     feh.append(compiled[i])
-for i in range(15,len(compiled)-1,11):
+for i in range(16,len(compiled)-1,12):
     names.append(compiled[i])
-for i in range(11,len(compiled)-1,11):
+for i in range(17,len(compiled)-1,12):
     objid.append(compiled[i])
     
-for i in range(16,len(compiled)-1,11):
+for i in range(18,len(compiled)-1,12):
     camcol.append(compiled[i])
-for i in range(17,len(compiled)-1,11):
+for i in range(19,len(compiled)-1,12):
     run.append(compiled[i])
-for i in range(18,len(compiled)-1,11):
+for i in range(20,len(compiled)-1,12):
     ids.append(compiled[i])
-for i in range(19,len(compiled)-1,11):
+for i in range(21,len(compiled)-1,12):
     plate.append(compiled[i])
-for i in range(20,len(compiled)-1,11):
+for i in range(22,len(compiled)-1,12):
     fiber.append(compiled[i])
-for i in range(21,len(compiled)-1,11):
+for i in range(23,len(compiled)-1,12):
     mjd.append(compiled[i])
 
-plt.show()
+tabs=[] #this will contain each fits file in one super-array
+
+for i in range(len(camcol)):
+    plateid=plate[i]
+    mjdid=mjd[i]
+    fiberid=fiber[i]
+    commands.getoutput('wget --content-disposition "http://api.sdss3.org/spectrum?plate='+plateid+'&fiber='+fiberid+'&mjd='+mjdid+'"')
+    
+    tab = pyfits.open(commands.getoutput("pwd")+'/spec-'+plateid.zfill(4)+'-'+mjdid+'-'+fiberid.zfill(4)+'.fits')
+    tabs.append(tab)
+    
