e6c9b7ba31f426f8d8e74d2fca6a92b00a32345f
Jeffrey Mei <mym226@nyu.edu>
2013-05-13 13:11:47 +0200

    New version also graphs one particular spectrum

--- graphcode.py
+++ graphcode.py
@@ -13,17 +13,49 @@ import os
 import sqldata_notmain
 array10=sqldata_notmain.sqldata(10)
 
-objs=array10[8]
-teff=array10[2]
-feh=array10[3]
-extinction=array10[1]
-ras=array10[12]
-decs=array10[13]
-magu=array10[14]
-magg=array10[15]
-magr=array10[16]
-magi=array10[17]
-magz=array10[18]
+wls=array10[1][1][0] #just one spectrum
+fluxes=array10[1][0][0]
+
+objs=array10[0][8]
+teff=array10[0][2]
+feh=array10[0][3]
+extinction=array10[0][1]
+ras=array10[0][12]
+decs=array10[0][13]
+magu=array10[0][14]
+magg=array10[0][15]
+magr=array10[0][16]
+magi=array10[0][17]
+magz=array10[0][18]
+c=['b']*len(objs)
+c[12]='r'
+s=[5]*len(objs)
+s[12]=100
+sa=np.array(s)
+ca=np.array(c)
+maggr=np.array(magg)-np.array(magr)
+magug=np.array(magu)-np.array(magg)
+magri=np.array(magr)-np.array(magi)
+
+plt.subplot(211)
+plt.scatter(np.array(ras),np.array(decs),s=sa, c=ca,marker='o')
+fig = plt.gcf()
+fig.set_size_inches(6,3)
+plt.xlabel("ra")
+plt.ylabel("dec")
+
+plt.subplot(212)
+plt.step(wls,fluxes)
+plt.xlabel("wavelengths (A)")
+plt.ylabel("flux (E-17 ergs/s/cm^2/A)")
+fig = plt.gcf()
+fig.set_size_inches(6,3)
+
+plt.show()
+
+
+
+'''
 
 s=[1,2,3,8,12,13,14,15,16,17,18]
 names=["extinction (mag)","TEff (K)","FeH","obj ID", "RA", "Dec", "mag_u","mag_g","mag_r","mag_i","mag_z"]
@@ -37,3 +69,4 @@ for i in range(len(s)):
 			plt.show()
 			#plt.savefig(str(str(i)+"-",str(j)+".png"))
 			
+'''
