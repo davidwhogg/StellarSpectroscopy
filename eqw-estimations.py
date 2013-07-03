36043680eae17e5695737b423f20b522cda07c0f
Jeffrey Mei <mym226@nyu.edu>
2013-07-03 16:10:44 +0200

    rewrote with function syntax

--- eqw-estimations.py
+++ eqw-estimations.py
@@ -1,3 +1,5 @@
+import gc
+gc.enable()
 import numpy as np
 import os
 from pylab import *
@@ -11,10 +13,9 @@ directory=commands.getoutput("pwd")
 sys.path.append(directory)
 import sqlcl
 import pickle
-#def eqw_estimation():
-for b in range(1):       
-    objids=[] #this one is left empty, ignore
 
+#Get the data via SQL query (we really only need plate-mjd-fiber for now)
+def sort_data():
     objids=[]
     extinction=[]
     hbflux=[]
@@ -37,7 +38,7 @@ for b in range(1):
 
     array1=[objids,extinction,hbflux,hbreqw,hbcont,hdflux,hdreqw,hdcont,objs,plate,fiber,mjd,\
             n2flux,n2reqw,n2cont,magg,hgflux,hgreqw,hgcont]
-#and p.extinction_g between "+str(exta)+" AND "+str(extb)+ ##
+    #and p.extinction_g between "+str(exta)+" AND "+str(extb)+ ##
     query = "SELECT p.objID, \
     p.extinction_g, g.h_beta_reqw_err, g.h_beta_reqw, g.h_beta_cont, g.h_delta_reqw_err, g.h_delta_reqw, g.h_delta_cont, \
     p.obj, s.plate, s.fiberID, s.mjd, g.h_alpha_flux, g.h_alpha_reqw, g.h_alpha_cont, \
@@ -45,7 +46,6 @@ for b in range(1):
     FROM PhotoObj AS p \
     JOIN SpecObj as s ON s.specobjID=p.specobjID \
     JOIN galSpecLine as g on s.specobjID=g.specobjID \
-    JOIN galSpecInfo as info on s.specObjID=info.specobjID \
     WHERE psfMag_r BETWEEN 15.0 and 19.0 \
     and p.type=6  \
      and  dbo.fPhotoStatus('PRIMARY')>0 and dbo.fPhotoFlags('STATIONARY')>0 \
@@ -68,72 +68,72 @@ for b in range(1):
     AND (psfMag_i-psfmag_z) between 0.02-0.08 and 0.02+0.08"
     alldata=sqlcl.query(query).read()
     interim=alldata.replace("\n",",")
+    nent=19 #(number of query columns)
     compiled=interim.split(",")
-    for i in range(20,len(compiled)-1,19):
-        extinction.append(float(compiled[i]))
-    for i in range(21,len(compiled)-1,19):
-        hbflux.append(float(compiled[i]))
-    for i in range(22,len(compiled)-1,19):
-        hbreqw.append(float(compiled[i]))
-    for i in range(23,len(compiled)-1,19):
-        hbcont.append((compiled[i]))
-    for i in range(24,len(compiled)-1,19):
-        hdflux.append(float(compiled[i]))
-    for i in range(25,len(compiled)-1,19):
-        hdreqw.append(float(compiled[i]))
-    for i in range(26,len(compiled)-1,19):
-        hdcont.append(float(compiled[i]))
-    for i in range(27,len(compiled)-1,19):
-        objs.append(float(compiled[i]))
-    for i in range(28,len(compiled)-1,19):
-        plate.append(int(compiled[i]))
-    for i in range(29,len(compiled)-1,19):
-        fiber.append(int(compiled[i]))
-    for i in range(30,len(compiled)-1,19):
-        mjd.append(int(compiled[i]))
-    for i in range(31,len(compiled)-1,19):
-        n2flux.append(float(compiled[i]))
-    for i in range(32,len(compiled)-1,19):
-        n2reqw.append(float(compiled[i]))
-    for i in range(33,len(compiled)-1,19):
-        n2cont.append(float(compiled[i]))
-    for i in range(34,len(compiled)-1,19):
-        magg.append((compiled[i]))
-    for i in range(35,len(compiled)-1,19):
-        hgflux.append(float(compiled[i]))
-    for i in range(36,len(compiled)-1,19):
-        hgreqw.append(float(compiled[i]))
-    for i in range(37,len(compiled)-1,19):
-        hgcont.append(float(compiled[i]))
-    print len(plate)
 
+    #sort all values into arrays:
+    for j in range(nent): 
+        for i in range(j+nent,len(compiled)-1,nent):
+            array1[j].append(float(compiled[i])) #note, plate/mjd/fiber should be int
+
+    #some of the values should be type(int):
+    for i in range(len(plate)): 
+            plate[i]=int(plate[i])
+            mjd[i]=int(mjd[i])
+            fiber[i]=int(fiber[i])
+
+    return plate, mjd, fiber
+
+plate, mjd, fiber = sort_data()
+print len(plate) #check number of stars surveyed
+
+
+
+
+#######Download fits files##########
+
+def downloadfits():
+    plateid=plate[i]
+    mjdid=mjd[i]
+    fiberid=fiber[i]
+    commands.getoutput('wget --content-disposition "http://api.sdss3.org/spectrum?plate='+str(plateid)+'&fiber='+str(fiberid)+'&mjd='+str(mjdid)+'"')
+    print "command success", i
+    return
+
+#downloadfits() #can be commented out if already downloaded
+
+
+
+
+########take flux, wl data from fits files##########
+def getdata():
+    
     tabs=[] #this will contain each fits file in one super-array
     fluxes=[]
     sn2s=[]
     errormags=[]
     wls=[]
-    array2=[fluxes,wls,sn2s,errormags]
-    for i in range(len(plate)) :
+    for i in range(4700,len(plate)):
         plateid=plate[i]
         mjdid=mjd[i]
         fiberid=fiber[i]
-        commands.getoutput('wget --content-disposition "http://api.sdss3.org/spectrum?plate='+str(plateid)+'&fiber='+str(fiberid)+'&mjd='+str(mjdid)+'"')
-        print "command success", i
-        '''tab = pyfits.open(commands.getoutput("pwd")+'/spec-'+str(plateid).zfill(4)+'-'+str(mjdid)+'-'+str(fiberid).zfill(4)+'.fits')
+        tab = pyfits.open(commands.getoutput("pwd")+'/spec-'+str(plateid).zfill(4)+'-'+str(mjdid)+'-'+str(fiberid).zfill(4)+'.fits')
+        gc.collect()
         print "tab success", i
         tabs.append(tab)
-        if type(tabs[i][2].data.field(63)[0])==float32: #distinguish SDSS,BOSS
-            zm= tabs[i][2].data.field(63)[0] #redshift
+        j=i-2700
+        if type(tabs[j][2].data.field(63)[0])==float32: #distinguish SDSS,BOSS
+            zm= tabs[j][2].data.field(63)[0] #redshift
             print i, "63"            
-        elif type(tabs[i][2].data.field(37)[0])==float32:
-            zm= taba[i][2].data.field(37)[0]
+        elif type(tabs[j][2].data.field(37)[0])==float32:
+            zm= tabs[j][2].data.field(37)[0]
             print i, "37"
         else:
             print "error"
             
-        flux=tabs[i][1].data.field(0) #flux
+        flux=tabs[j][1].data.field(0) #flux
         fluxes.append(flux)
-        loglam=tabs[i][1].data.field(1)
+        loglam=tabs[j][1].data.field(1)
         loglam=np.array(loglam)
         lam=10**loglam
         lamcor=zeros(len(lam))
@@ -141,17 +141,32 @@ for b in range(1):
             lamcor[k]=float(lam[k])/float((1+zm)) 
             
         wls.append(lamcor)
-        sn2=tabs[i][2].data.field(6)[0]+tabs[i][2].data.field(7)[0] #one of these entries is 0 always
+        sn2=tabs[j][2].data.field(6)[0]+tabs[j][2].data.field(7)[0] #one of these entries is 0 always
         sn2s.append(sn2)
         errormag=1/sn2
         errormags.append(errormag)
+        tab.close()
+        del tab
+        gc.collect()
+    return wls, fluxes, sn2s
 
-    #below section graphs the spectrum
+wls, fluxes, sn2s = getdata()
+
+
+
+
+#####Calculate cont, eqw, flux values###########
+def calc():
     betadata=[]
     gammadata=[]
     deltadata=[]
+
+    f=open("ext1", "rb")
+    data=pickle.load(f)
+    f.close()
+    
     grouped_data=[deltadata,gammadata,betadata]
-    for z in range(len(plate)):
+    for z in range(len(plate)-4700):
         s = UnivariateSpline(wls[z], fluxes[z], k=3, s=0)
         xs=linspace(min(wls[z]),max(wls[z]),len(wls[z])*10)
         
@@ -160,7 +175,6 @@ for b in range(1):
         gamma= [x for x in xs if x>4240 and x<4441]
         delta= [x for x in xs if x>4002 and x<4202]
         windows=[delta, gamma, beta]
-
 
         for w in range(len(peaks)): #for each peak location...
             y=s(windows[w])
@@ -176,42 +190,44 @@ for b in range(1):
                     #len flux_domain = len ys_corr
                     flux = 0.5*(flux_domain[1]-flux_domain[0])*(2*sum(ys_corr)-ys_corr[0]-ys_corr[len(ys_corr)-1]) #trap rule
                     eqw = flux/cont1
-                    grouped_data[w].append([cont1, flux, eqw, peak_loc,])
+                    data[w].append([cont1, flux, eqw, peak_loc,]) #grouped_data
                     print "ok done", z, w
-            
-            plt.fill_between(flux_domain, ys, cont1, color='gray', alpha=0.5)
+    f2=open("ext2","wb")    
+    pickle.dump(data,f2)
+    f2.close()
 
-        plt.xlim(4000,5000)
-        plt.step(xs,s(xs),'b', linewidth=0.5, alpha=1) 
-        plt.xlabel("wavelengths (A)")
-        plt.ylabel("flux (E-17 ergs/s/cm^2/A)")
-        for j in range(len(peaks)): #integral neighborhoods
-            plt.axvline(x=grouped_data[j][z][3]+10, color='g')
-            plt.axvline(x=grouped_data[j][z][3]-10, color='g')
-            plt.axvline(x=grouped_data[j][z][3], color='b')
-        plt.axvline(x=4761) #cont neighborhoods
-        plt.axvline(x=4962)
-        plt.axvline(x=4240)
-        plt.axvline(x=4441)
-        plt.axvline(x=4002)
-        plt.axvline(x=4202)
-        ##################
-        plt.plot(np.array([4761,4961]),np.array([grouped_data[2][z][0]]*2), color='k')
-        plt.plot(np.array([4002,4202]),np.array([grouped_data[0][z][0]]*2), color='k')
-        plt.plot(np.array([4240,4440]),np.array([grouped_data[1][z][0]]*2), color='k')
+    ##plot    
+    '''plt.fill_between(flux_domain, ys, cont1, color='gray', alpha=0.5)
 
-        plt.title("Spectrum for extinction ="+ str(extinction[z]))
-        print z, "blue flux, cont, reqw @ h-b 4861 is", hbflux[z], hbcont[z], hbreqw[z]
-        print z, "blue flux, cont, reqw @ h-d 4102 is", hdflux[z], hdcont[z], hdreqw[z]
-        print z, "blue flux, cont, reqw @ h-a 6565 is", n2flux[z], n2cont[z], n2reqw[z]
-        print z, "blue flux, cont, reqw @ h-gamma 4340 is", hgflux[z], hgcont[z], hgreqw[z]
-        plt.grid(True)
-        #print grouped_data
-        plt.show()'''
-#f=open("ext1", "wb")
-#pickle.dump(grouped_data,f)
-#f.close()
-    #return grouped_data
+    plt.xlim(4000,5000)
+    plt.step(xs,s(xs),'b', linewidth=0.5, alpha=1) 
+    plt.xlabel("wavelengths (A)")
+    plt.ylabel("flux (E-17 ergs/s/cm^2/A)")
+    for j in range(len(peaks)): #integral neighborhoods
+        plt.axvline(x=grouped_data[j][z][3]+10, color='g')
+        plt.axvline(x=grouped_data[j][z][3]-10, color='g')
+        plt.axvline(x=grouped_data[j][z][3], color='b')
+    plt.axvline(x=4761) #cont neighborhoods
+    plt.axvline(x=4962)
+    plt.axvline(x=4240)
+    plt.axvline(x=4441)
+    plt.axvline(x=4002)
+    plt.axvline(x=4202)
+    ##################
+    plt.plot(np.array([4761,4961]),np.array([grouped_data[2][z][0]]*2), color='k')
+    plt.plot(np.array([4002,4202]),np.array([grouped_data[0][z][0]]*2), color='k')
+    plt.plot(np.array([4240,4440]),np.array([grouped_data[1][z][0]]*2), color='k')
+
+    plt.title("Spectrum for extinction ="+ str(extinction[z]))
+    print z, "blue flux, cont, reqw @ h-b 4861 is", hbflux[z], hbcont[z], hbreqw[z]
+    print z, "blue flux, cont, reqw @ h-d 4102 is", hdflux[z], hdcont[z], hdreqw[z]
+    print z, "blue flux, cont, reqw @ h-a 6565 is", n2flux[z], n2cont[z], n2reqw[z]
+    print z, "blue flux, cont, reqw @ h-gamma 4340 is", hgflux[z], hgcont[z], hgreqw[z]
+    plt.grid(True)
+    #print grouped_data
+    plt.show()'''
+
+#return grouped_data
 ## grouped_data is separated into 3 columns, one for each peak location
 ## grouped_data[i] is further split into n entries for the n value chosen 
 ## grouped_data[i][j] is split into [0]=cont, [1]=flux, [2]=eqw, [3]=peak location
