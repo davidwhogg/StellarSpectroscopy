29e6d337d09163bb69c958863036a0276f1d965a
Jeffrey Mei <mym226@nyu.edu>
2013-07-17 16:08:29 +0200

    improved efficiency by removing for loops

--- eqw-estimate_new.py
+++ eqw-estimate_new.py
@@ -118,7 +118,7 @@ def getdata():
     badpoints=[]
 
     ### use this by default
-    for i in range(2700,len(plate)):  #cf 125, 178, 222
+    for i in range(2700):#2700,len(plate)):  #cf 125, 178, 222
         plateid=plate[i]
         mjdid=mjd[i]
         fiberid=fiber[i]
@@ -126,7 +126,7 @@ def getdata():
         tab = pyfits.open(commands.getoutput("pwd")+'/spec-'+str(plateid).zfill(4)+'-'+str(mjdid)+'-'+str(fiberid).zfill(4)+'.fits')
         print "tab success", i
         tabs.append(tab)
-        j=i-2700 #-2700 ###########
+        j=i#-2700 #-2700 ###########
         if type(tabs[j][2].data.field(63)[0])==float32: #distinguish SDSS,BOSS
             zm= tabs[j][2].data.field(63)[0] #redshift
             print i, "63"            
@@ -158,7 +158,7 @@ def getdata():
                 ivars[j][v]=1/sqrt(ivars[j][v])
             elif ivars[j][v]==0:
                 bad.append(wls[j][v])
-                ivars[j][v]=100
+                ivars[j][v]=np.inf
         badpoints.append(bad)
         tab.close()
     return wls, fluxes, sn2s, ivars, badpoints
@@ -171,59 +171,66 @@ wls, fluxes, sn2s, ivars, badpoints = getdata()
 ##### Calculate cont, eqw, flux values ###########
 def calc():
 
-    f=open("tight", "rb")
-    data=pickle.load(f)
-    f.close()
-    #data = [[],[],[]]
-    for z in range(len(plate)-2700): #len(plate)-2700
+    #f=open("tightnew", "rb")
+    #data=pickle.load(f)
+    #f.close()
+    data = [[],[],[],[],[],[],[],[]]
+    for z in range(2700):#len(plate)-2700): #len(plate)-2700
         s = UnivariateSpline(wls[z], fluxes[z], k=3, s=0)
         xs=linspace(min(wls[z]),max(wls[z]),len(wls[z])*10)
-        
-        peaks=[4102, 4340, 4861] # Only using h-delta, h-gamma, h-beta
-        
-        ys_red = s(wls[z])
-        
+        # peaks are [hd, hg, hb, He-I,   K,    H,   TiO,  O-I]
+        peaks=[4102, 4340, 4861, 3890, 3934, 3970, 5582, 6306] # Only using h-delta, h-gamma, h-beta
+                
+
         for w in range(len(peaks)): #for each peak location..
-            cont_zone=[x for x in xs if x>peaks[w]-100 and x<peaks[w]+100 and x not in badpoints[z]]
+            good = (ivars[z]!=np.inf)
+            cont_indx = good*(wls[z]>peaks[w]-100)*(wls[z]<peaks[w]+100)
+            flux_indx = (wls[z]>peaks[w]-10)*(wls[z]<peaks[w]+10)
+
+            cont_zone=wls[z][(cont_indx*(True-flux_indx))]
+
+            
+            #cont_zone=[x for x in wls[z] if x>peaks[w]-100 and x<peaks[w]+100 and x not in badpoints[z]]
             cont_est=s(cont_zone)
             cont1=np.median(cont_est) #Cont value
- 
-            peak_loc_finder=[x for x in xs if x>peaks[w]-2.5 and x<peaks[w]+2.5]
-            peak_locs=s(peak_loc_finder)
-            for q in range(len(peak_locs)):
-                if peak_locs[q]==min(peak_locs):
-                    peak_loc=peak_loc_finder[q] #peak location found.
-            flux_domain = [x for x in xs if x<peak_loc+10 and x>peak_loc-10] #neighborhood of 20A
-            ys = s(flux_domain)
+
+
+            bad = True-good
+            fail_indx = bad*(wls[z]>peaks[w]-100)*(wls[z]<peaks[w]+100)
+            fail_flag=wls[z][fail_indx]
+            #fail_flag=[x for x in wls[z] if x>peaks[w]-100 and x<peaks[w]+100 and x in badpoints[z]]
+
+
+
+            flux_zone=wls[z][flux_indx]
+            ys = s(flux_zone)
 
             ###take ivars for the cont range
-            errors = np.array([ivars[z][x] for x in range(len(ivars[z])) \
-                               if wls[z][x]<peaks[w]+100 and wls[z][x]>peaks[w]-100 \
-                               and wls[z][x] not in badpoints[z]])
+            errors = np.array(ivars[z][cont_indx])
+                               
             cont_err = np.sqrt(sum(errors**2))/len(errors)
             
             ## integral:
             ys_corr = ys-cont1 #ready for integration
             #len flux_domain = len ys_corr
-            flux = 0.5*(flux_domain[1]-flux_domain[0])*(2*sum(ys_corr)-ys_corr[0]-ys_corr[len(ys_corr)-1]) #trap rule
+            flux = 0.5*(flux_zone[1]-flux_zone[0])*(2*sum(ys_corr)-ys_corr[0]-ys_corr[len(ys_corr)-1]) #trap rule
     
             #calculate flux error: weighted sum with weights=stepsize
-            flux_errors = np.array([ivars[z][x] for x in range(len(ivars[z]))\
-                                    if wls[z][x]<peak_loc+10 and wls[z][x]>peak_loc-10\
-                                    and wls[z][x]])
-            f_errors_squared = sum(flux_errors**2) #sum squares
-            f_error_w = 4*(f_errors_squared)-3*(flux_errors[0]**2+flux_errors[len(flux_errors)-1]**2) #apply weights; 1 + 4 + ... + 4 + 1
+            flux_errors = np.array(ivars[z][flux_indx])
+            
+            f_errors_squared = sum(flux_errors[1:-1]**2) #sum squares, not first or last value
+            f_error_w = 4*(f_errors_squared)+(flux_errors[0]**2+flux_errors[len(flux_errors)-1]**2) #apply weights; 1 + 4 + ... + 4 + 1
             flux_err = np.sqrt(f_error_w)*(wls[z][1]-wls[z][0]) #weight with step-size            
 
             eqw = flux/cont1
-            zz=z+2700 #or z+2700
-            data[w].append([cont1, cont_err, flux, flux_err, eqw, peak_loc, extinction[zz], plate[zz], mjd[zz], fiber[zz], str(int(objid[zz]))]) #grouped_data
+            zz=z#+2700 #or z+2700
+            data[w].append([cont1, cont_err, flux, flux_err, eqw, extinction[zz], plate[zz], mjd[zz], fiber[zz], str(int(objid[zz])), len(fail_flag)]) #grouped_data
             print "ok done", z, w
     return data #grouped_data and also 220
 
-grouped_data = calc() #len(tabs)?  ##grouped_data
-f2=open("tight","wb")    
-pickle.dump(grouped_data,f2) #grouped_data
+data = calc() #save data to file
+f2=open("newlines","wb")    
+pickle.dump(data,f2) 
 f2.close()
 
 ########### Plot spectra ######## (need grouped_data) - can only plot 
